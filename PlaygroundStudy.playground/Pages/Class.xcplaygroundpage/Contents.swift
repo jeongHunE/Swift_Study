0e5e247168cbbd56b12f96953ca8d01b7495302d
jeungHunLee <ksjs1111@gmail.com>
2022-05-17 10:13:03 +0900

    SWift 클래스

--- PlaygroundStudy.playground/Pages/Class.xcplaygroundpage/Contents.swift
+++ PlaygroundStudy.playground/Pages/Class.xcplaygroundpage/Contents.swift
@@ -1,7 +1,32 @@
-//: [Previous](@previous)
+//클래스 정의
+class Person {
+    var name: String = "jeunghun"
+    var age: Int = 24
+    var height: Double = 0.0
+    var weight: Double = 0.0
+    
+    //디이니셜라이저
+    deinit {
+        print("Person 클래스의 인스턴스가 소멸됩니다.")
+    }
+}
 
-import Foundation
+var jeunghun: Person = Person()     //인스턴스 생성
+print(jeunghun.name)
+print(jeunghun.age)
+jeunghun.height = 173.3
+jeunghun.weight = 60.8
+print(jeunghun.height)
+print(jeunghun.weight)
 
-var greeting = "Hello, playground"
+var joe: Person? = Person()
+joe = nil   //메모리 해제
 
-//: [Next](@next)
+//식별 연산자 사용
+var mark: Person = Person()
+let friend: Person = mark
+let anotherFriend: Person = Person()
+
+print(mark === friend)
+print(mark === anotherFriend)
+print(friend !== anotherFriend)
