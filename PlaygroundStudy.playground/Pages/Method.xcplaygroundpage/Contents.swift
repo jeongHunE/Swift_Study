6596843b3f63cb433df6044f2fc0cc7ebed1ec4b
jeungHunLee <ksjs1111@gmail.com>
2022-06-07 16:15:19 +0900

    Swift 메서드

--- PlaygroundStudy.playground/Pages/Method.xcplaygroundpage/Contents.swift
+++ PlaygroundStudy.playground/Pages/Method.xcplaygroundpage/Contents.swift
@@ -92,7 +92,7 @@ struct PhotoSize {
     static var size: Int = 100
     
     static func zoomIn() {
-        self.size =  200    //self -> PhotoSize
+        self.size =  200    //self: 자기 자신 타입
     }
 }
 
