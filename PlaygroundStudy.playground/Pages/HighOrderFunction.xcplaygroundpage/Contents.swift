0f99e09c4d4ce0b343d34a3b7cd92259d663fcd5
jeungHunLee <ksjs1111@gmail.com>
2022-06-16 15:14:40 +0900

    Swift 고차함수

--- PlaygroundStudy.playground/Pages/HighOrderFunction.xcplaygroundpage/Contents.swift
+++ PlaygroundStudy.playground/Pages/HighOrderFunction.xcplaygroundpage/Contents.swift
@@ -36,3 +36,38 @@ print(evenNum)      //[2, 4, 6]
 var oddNum: [Int] = numbers.map { $0 + 5 }.filter { $0 % 2 == 1}    //numbers의 각 요소에 5씩 더한 후 홀수 추출
 
 print(oddNum)       //[7, 9, 11]
+
+//reduce(리듀스)
+//연산 후 결과 값을 반환하는 형태의 리듀스
+var sum: Int = numbers.reduce(0, { (result: Int, next: Int) -> Int in
+    return result + next    //초기값이 1이고 numbers에 모든 요소를 더하는 리듀스
+})
+
+print(sum)      //21
+
+var mul: Int = numbers.reduce(1) {  //초기값이 1이고 numbers의 모든 요소를 곱하는 리듀스
+    $0 * $1     //후행 클로저, return 생략, 매개변수 생략
+}
+
+print(mul)  //720
+
+//연산 후 결과 같은 반환 하지 않고 초기값에 직접 연산을 수행하는 형태의 리듀스
+sum = numbers.reduce(into: 0, { (result: inout Int, next: Int) in
+    result += next  //값을 반환하지 않고 inout으로 직접 연산 수행
+})
+
+print(sum)      //21
+
+mul = numbers.reduce(into: 1) {
+    $0 *= $1    //후행 클로저, return 생략, 매개변수 생략
+}
+
+print(mul)      //720
+
+//소문자 String을 대문자 String으로 바꾸기
+let language: [String] = ["Swift", "Kotlin", "Objective-C", "Java", "Python"]
+
+var upperString: [String] = language.reduce(into: []) {
+    $0.append($1.uppercased())
+}
+print(upperString)      //["SWIFT", "KOTLIN", "OBJECTIVE-C", "JAVA", "PYTHON"]
