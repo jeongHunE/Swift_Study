f780840437ff1b52ff8634f625db9627bbb68fbd
jeungHunLee <ksjs1111@gmail.com>
2022-06-18 19:59:58 +0900

    Swift 빠른 종료

---
+++ PlaygroundStudy.playground/Pages/EarlyExit.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//빠른 종료(Early Exit)
+for i in 1...10 {
+    guard i % 2 == 0 else {     //guard 조건에 맞지 않으면 else문 실행
+        continue
+    }
+    print(i)
+}
+
+//옵셔널 바인딩을 사용한 guard 구문
+func introduce(_ person: [String: String]) -> Void {
+    guard let name: String = person["name"] else {
+        return
+    }
+    
+    print("저의 이름은 \(name) 입니다.")
+    
+    guard let residence: String = person["residence"] else {
+        return
+    }
+    
+    print("저는 \(residence)에 거주중 입니다.")
+    
+    guard let job: String = person["job"] else {
+        return
+    }
+    
+    print("저의 직업은 \(job) 입니다.")
+}
+
+var person: [String: String] = [String: String]()   //새로운 딕셔너리 생성
+person["name"] = "이철수"      //딕셔너리에 새로운 Key: Value 추가
+person["residence"] = "대한민국"
+person["job"] = "개발자"
+
+introduce(person)
