8a1f75136c4fa1f519edc903518c18c2c4d2368f
jeungHunLee <ksjs1111@gmail.com>
2022-06-21 15:59:03 +0900

    Swift 서브스크립트

--- PlaygroundStudy.playground/Pages/Subscript.xcplaygroundpage/Contents.swift
+++ PlaygroundStudy.playground/Pages/Subscript.xcplaygroundpage/Contents.swift
@@ -18,12 +18,44 @@ class School {
         }
     }
     
-    subscript(key: Int) -> Student? {   //key로 value를 반환하는 서브스크립트
-        if key < self.number {
-            return students[key]
+    //첫번째 서브스크립트
+    subscript(key: Int) -> Student? {   //key값에 해당하는 value를 반환
+        get {
+            if key < self.number {
+                return students[key]
+            }
+            return nil
         }
-        return nil
+        
+        //새로운 학생 추가
+        set {
+            guard var newStudent: Student = newValue else {
+                return
+            }
+            
+            var number = key
+            
+            if number > self.number {
+                number = self.number
+                self.number += 1
+            }
+            
+            students[number] = newStudent
+        }
     }
+    
+    //두번째 서브스크립트
+    subscript(name: String) -> Int? {   //name에 해당하는 인스턴스의 key값 반환
+        get {
+            return self.students.filter { $0.1.name == name }.first?.key
+        }
+    }
+    
+    //세번째 서브스크립트
+    subscript(key: Int, name: String) -> Student? { //key값과 name값에 해당하는 Student 인스턴스 반환
+        return self.students.filter { $0.0 == key && $0.1.name == name }.first?.value
+    }
+    
 }
 
 let highSchool: School = School()
@@ -32,8 +64,28 @@ var studentLee: Student = Student(name: "이철수")
 var studentKim: Student = Student(name: "김영희")
 var studentSon: Student = Student(name: "손민준")
 var studentPark: Student = Student(name: "박현우")
+var studentHong: Student = Student(name: "홍수민")
 
 highSchool.addStudents(students: studentLee, studentKim, studentSon, studentPark)
 
-let aStudent: Student? = highSchool[1]
+let aStudent: Student? = highSchool[1]  //첫번째 서브스크립트 사용
 print(aStudent)
+
+highSchool[8] = studentHong     //첫번째 서브스크립트의 set 메서드 사용
+
+print(highSchool["홍수민"])        //두번째 서브스크립트 사용
+print(highSchool["이철수"])        //두번째 서브스크립트 사용
+print(highSchool[1, "김영희"])     //세번째 서브스크립트 사용
+
+//타입 서브스크립트
+enum BaekjoonTier: Int {
+    case bronze = 1
+    case silver, gold
+    
+    static subscript(tier: Int) -> BaekjoonTier? {  //타입 서브스크립트
+        return Self(rawValue: tier) //원시 값에 해당하는 항목 반환
+    }
+}
+
+let myTier: BaekjoonTier? = BaekjoonTier[2]
+print(myTier)
