f5968affff0ed643e8fb4b485cf041d6591cd6dd
jeungHunLee <ksjs1111@gmail.com>
2023-06-25 21:43:54 +0900

    Nested Type

--- PlaygroundStudy.playground/Pages/Nested_Type.xcplaygroundpage/Contents.swift
+++ PlaygroundStudy.playground/Pages/Nested_Type.xcplaygroundpage/Contents.swift
@@ -14,11 +14,14 @@ class Person {
 }
 
 let personLee: Person = Person(name: "이순신", sex: .male)
-print(personLee.name)
+print(personLee.name)    //이순신
 
 switch personLee.sex {
 case .male:
-    print("남성입니다.")
+    print("남성입니다.")    //남성입니다.
 case .female:
-    print("여성입니다.")
+    print("여성입니다.")    //여성입니다.
 }
+
+let nestedType: Person.Sex = personLee.sex
+print(nestedType)    //male
